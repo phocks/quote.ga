d47bcd5786f40e4482a511e35e14d8bdd35c3c36
Joshua Byrd <phocks@gmail.com>
2022-01-27 18:13:18 +1000

    Some favicons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	import {user} from "$lib/sessionStore"
-	import {supabase} from "$lib/db"
-	import Auth from "$lib/components/Auth.svelte"
-	import Profile from "$lib/components/Profile.svelte"
-	user.set(supabase.auth.user())
-	supabase.auth.onAuthStateChange((_, session) => {
-		user.set(session.user)
-	})
+  import { user } from "$lib/sessionStore";
+  import { supabase } from "$lib/db";
+  import Auth from "$lib/components/Auth.svelte";
+  import Profile from "$lib/components/Profile.svelte";
+  user.set(supabase.auth.user());
+  supabase.auth.onAuthStateChange((_, session) => {
+    user.set(session.user);
+  });
 </script>
 
+<svelte:head>
+  <!-- <title>Quote</title> -->
+</svelte:head>
+
 <div class="container" style="padding: 50px 0 100px 0;">
-	{#if $user}
-			<Profile />
-	{:else}
-			<Auth />
-	{/if}
+  {#if $user}
+    <Profile />
+  {:else}
+    <Auth />
+  {/if}
 </div>
